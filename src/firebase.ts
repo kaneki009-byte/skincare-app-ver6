7ddb9482f915ac90f0af06a29a0966c725f8f612
かねきこうへい <kanekikouhei@kanekikouheinoMacBook-Air.local>
2025-10-20 10:28:27 +0900

    use shared firebase account

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, signInAnonymously } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrNtw-yDO4vRsIqKEjqgD2Qw4OHO9lgNs",
-  authDomain: "skin-care-tracker-e7b88.firebaseapp.com",
-  projectId: "skin-care-tracker-e7b88",
-  storageBucket: "skin-care-tracker-e7b88.firebasestorage.app",
-  messagingSenderId: "694240538671",
-  appId: "1:694240538671:web:642a1068e117727aa656da"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 const auth = getAuth(app);
-signInAnonymously(auth)
-  .then(() => console.log("匿名ログイン成功"))
-  .catch((e) => console.error("匿名ログイン失敗:", e));
+signInWithEmailAndPassword(auth, "nurse@test.com", "123456")
+  .then(() => console.log("共通アカウントでログイン成功"))
+  .catch((e) => console.error("ログイン失敗:", e));
+
